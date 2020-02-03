9156a15605af0b4f54ff63bf310ea3ab4443e394
Diego Vasquez <botellongo69@outlook.com>
2020-02-03 14:46:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,13 @@
 class Anagram
   attr_accessor :word
   
-  def initialization(word)
+  def initialize(word)
     @word = word
   end
   
   def match(word_array)
-    split_word = word_array.split
+    split_word = @word.split('')
     
-    word_array.each do |word|
-      splitted_word = word.split
-      if split_word.sort = splitted_word.sort
-        word
-      end
-    end
+    word_array.select {|word| word.split('').sort == split_word.sort}
   end
 end
