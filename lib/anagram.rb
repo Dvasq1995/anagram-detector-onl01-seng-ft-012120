8c7f8a798acf0ebcc5eb02098558f16de2f26307
Diego Vasquez <botellongo69@outlook.com>
2020-02-03 14:30:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialization(word)
+    @word = word
+  end
+  
+  def match(word_array)
+    split_word = word_array.split
+    
+    word_array.each do |word|
+      splitted_word = word.split
+      if split_word.sort = splitted_word.sort
+        word
+      end
+    end
+  end
+end
